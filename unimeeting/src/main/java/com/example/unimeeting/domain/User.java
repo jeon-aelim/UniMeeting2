6b0b69507759c62dc49895feb2831528065eab52
hapalpal <ehddnsla0102@naver.com>
2023-11-07 20:29:13 +0900

    t

--- unimeeting/src/main/java/com/example/unimeeting/domain/User.java
+++ unimeeting/src/main/java/com/example/unimeeting/domain/User.java
@@ -1,4 +1,21 @@
 package com.example.unimeeting.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+@Entity
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idx;
+    private String user_id;
+    private String password;
+    private String email;
+    private String category;
+    private String phone_number;
+    private String nickname;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Board> boards;
 }
--- unimeeting/src/main/java/com/example/unimeeting/repository/UserRepository.java
+++ unimeeting/src/main/java/com/example/unimeeting/repository/UserRepository.java
@@ -1,4 +1,20 @@
 package com.example.unimeeting.repository;
 
-public interface UserRepository {
+import com.example.unimeeting.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    //public List<User> findById();
+
+
+
+
+
+
+
+
+
 }
