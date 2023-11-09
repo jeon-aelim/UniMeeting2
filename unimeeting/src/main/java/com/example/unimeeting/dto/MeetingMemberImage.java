994dae3dcdd278758fcb783b1e7d177217b749f5
mang0206 <alswosp0206@naver.com>
2023-11-09 18:05:16 +0900

    create dto

---
+++ unimeeting/src/main/java/com/example/unimeeting/dto/MeetingMemberImage.java
@@ -0,0 +1,29 @@
+package com.example.unimeeting.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MeetingMemberImage {
+  private int idx;
+
+}
