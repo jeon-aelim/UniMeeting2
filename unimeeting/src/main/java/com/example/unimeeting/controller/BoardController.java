ebf8f34dd753a80889e529e09500a8406257e2e8
hapalpal <ehddnsla0102@naver.com>
2023-11-14 11:17:25 +0900

    Merge issue

--- unimeeting/src/main/java/com/example/unimeeting/controller/BoardController.java
+++ unimeeting/src/main/java/com/example/unimeeting/controller/BoardController.java
@@ -2,9 +2,11 @@ package com.example.unimeeting.controller;
 
 import com.example.unimeeting.domain.Board;
 import com.example.unimeeting.service.BoardService;
+<<<<<<< HEAD
 import lombok.RequiredArgsConstructor;
+=======
+>>>>>>> c950df6 (ignore idea)
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,10 @@ import java.util.List;
 @RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
 
     //=============글 목 ===========//
     @GetMapping("/boards/{type}")
@@ -50,11 +56,8 @@ public class BoardController {
     //=============글 삭제 ==============//
     @DeleteMapping("/boards/{id}")
     public ResponseEntity<String> deleteBoard(@PathVariable int id) {
-
         boardService.deleteById(id);
         return ResponseEntity.ok("Board deleted successfully");
     }
-
-    // Helper 메서드: 게시글 작성자 확인
 
 }
