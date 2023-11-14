5cc4ff66c7512ca4443d455089ebeeeaa49f3568
jeon-aelim <devaelim@gmail.com>
2023-11-14 11:50:39 +0900

    fix error

--- unimeeting/src/main/java/com/example/unimeeting/controller/MeetingController.java
+++ unimeeting/src/main/java/com/example/unimeeting/controller/MeetingController.java
@@ -1,12 +1,16 @@
 package com.example.unimeeting.controller;
 
 import com.example.unimeeting.domain.Meeting;
+import com.example.unimeeting.dto.MeetingResponse;
 import com.example.unimeeting.service.MeetingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("meetings")
+@RequestMapping("/meetings")
 public class MeetingController {
 
     private final MeetingService meetingService;
@@ -16,10 +20,12 @@ public class MeetingController {
     }
      // security를 사용해 user 정보 받기. (user마다 response하는 값이 다름.)
 
-//    @GetMapping("/{idx}")
-//    public ResponseEntity<MeetingViewResponse> getOne(@PathVariable int id){
-//        Meeting meeting = meetingService.findById(id);
-//
-//
-//    }
+    @GetMapping("/{idx}")
+    public ResponseEntity<MeetingResponse> getOne(@PathVariable int id){
+        Meeting meeting = meetingService.findById(id);
+
+
+    }
+
+
 }
