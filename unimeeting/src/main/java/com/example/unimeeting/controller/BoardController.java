c950df6efa9f0e81d3731a957ee56b9333359b5d
hapalpal <ehddnsla0102@naver.com>
2023-11-14 10:53:44 +0900

    ignore idea

--- unimeeting/src/main/java/com/example/unimeeting/controller/BoardController.java
+++ unimeeting/src/main/java/com/example/unimeeting/controller/BoardController.java
@@ -2,9 +2,7 @@ package com.example.unimeeting.controller;
 
 import com.example.unimeeting.domain.Board;
 import com.example.unimeeting.service.BoardService;
-import com.example.unimeeting.service.CurrentUserProvider;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +14,11 @@ import java.util.List;
 @RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
-    private final CurrentUserProvider currentUserProvider;
-    @Autowired
-    public BoardController(BoardService boardService, CurrentUserProvider currentUserProvider){
-        this.boardService=boardService;
-        this.currentUserProvider = currentUserProvider;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
     }
+
     //=============글 목 ===========//
     @GetMapping("/boards/{type}")
     public ResponseEntity<List<Board>> getAllBoards(@PathVariable String type, @RequestParam(required = false) String search) {
@@ -47,26 +44,14 @@ public class BoardController {
     //=============글 수정 ===========//
     @PutMapping("/boards/{id}")
     public ResponseEntity<String> updateBoard(@PathVariable int id, @RequestBody Board board) {
-        if (!isAuthorized(id, currentUserProvider.getCurrentUserNickname())) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("권한이 없습니다");
-        }
         boardService.update(id, board);
         return ResponseEntity.ok("Board updated successfully");
     }
     //=============글 삭제 ==============//
     @DeleteMapping("/boards/{id}")
     public ResponseEntity<String> deleteBoard(@PathVariable int id) {
-        if (!isAuthorized(id, currentUserProvider.getCurrentUserNickname())) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("권한이 없습니다.");
-        }
         boardService.deleteById(id);
         return ResponseEntity.ok("Board deleted successfully");
     }
 
-    // Helper 메서드: 게시글 작성자 확인
-    private boolean isAuthorized(int boardId, String writerNickname) {
-        // 여기에서는 SecurityContext를 통해 현재 사용자의 정보를 가져와 확인합니다.
-        String currentUserNickname = currentUserProvider.getCurrentUserNickname();
-        return currentUserNickname != null && currentUserNickname.equals(writerNickname);
-    }
 }
